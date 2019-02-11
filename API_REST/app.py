9de00d24ff11574849afb82ef767a863d5c38c71
v.sanchez <v.sanchez@indizen.com>
2019-02-11 11:39:45 +0100

    Código para crear una base de datos SQL y utilizar al API REST para acceder a ella

--- API_REST/app.py
+++ API_REST/app.py
@@ -6,6 +6,10 @@ from flask_restful import Resource, Api
 from secure_check import authenticate, identity
 from flask_jwt import JWT, jwt_required
 
+# Para realizar queries a base de datos
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecretkey"
 
@@ -15,15 +19,41 @@ api = Api(app)
 # Se crea un objeto JWT, se le pasa la app, authencate y indetity
 jwt = JWT(app, authenticate, identity)
 
+# BBDD
+# Crea una base de datos en el mismo directorio en el que está
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'date.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+# # Crea la base de datos conectada a la app
+# db = SQLAlchemy(app)
+# Migrate(app, db)
 
 puppies = []
 
+
+#########################################
+
+# Crear un modelo para la base de datos
+# class Puppy(db.model):
+#     name = db.Column(db.string(80), primary_key=True)
+#
+#     def __init__(self, name):
+#         self.name = name
+#
+#     # función que devuelve un json con el nombre de la mascota
+#     def json(self):
+#         return {"name": self.name}
+
+
+########################################
 
 class PuppyNames(Resource):
 
     # RECUPERAR UNA MASCOTA
     def get(self, name):
 
+        # PRIMER EJEMPLO - SIN BASE DE DATOS LEYENDO DE LA LISTA DE PUPPIES
         # Buscar una mascota por nombre recorriendo el diccionario
         for puppy in puppies:
             # Se accede al valor de la clave name y se compara con el nombre que entra
@@ -34,8 +64,19 @@ class PuppyNames(Resource):
         # Se le puede pasar el tipo de error que es
         return {"name": None}, 404
 
+        # SEGUNDO EJEMPLO - LEYENDO DE LA BASE DE DATOS SQLITE
+        # Realiza la query buscando por nombre la mascota
+        # puppy = Puppy.query.filter_by(name=name).first()
+        #
+        # if puppy:
+        #     return puppy.json()
+        # else:
+        #     return {"name": None}, 404
+
     # GUARDAR UNA MASCOTA
     def post(self, name):
+
+        # PRIMER EJEMPLO - SIN BASE DE DATOS GUARDANDO MASCOTA EN LA LISTA DE PUPPIES
         # Recibe un nombre y crea un objeto puppy
         puppy = {"name": name}
 
@@ -45,9 +86,17 @@ class PuppyNames(Resource):
         # Devuelve la mascota
         return puppy
 
+        # # SEGUNDO EJEMPLO - GUARDANDO EM LA BASE DE DATOS SQLITE
+        # puppy = Puppy(name=name)
+        # db.session.add(puppy)
+        # db.session.commit()
+
+        return puppy.json()
+
     # BORRAR UNA MASCOTA
     def delete(self, name):
 
+        # PRIMER EJEMPLO - BORRAR SIN BASE DE DATOS
         # Se recorre las mascotas obteniendo la mascota y su index dentro del diccionario
         for index, puppy in enumerate(puppies):
 
@@ -58,6 +107,12 @@ class PuppyNames(Resource):
 
         # Si no lo encuentra
         return {"note": "No se ha encontrado la mascota a borrar"}, 404
+
+        # # SEGUNDO EJEMPLO - BORRANDO DE LA BASE DE DATOS SQLITE
+        # # Realiza la query buscando por nombre la mascota
+        # puppy = Puppy.query.filter_by(name=name).first()
+        # db.session.delete(puppy)
+        # db.session.commit()
 
 
 class AllNames(Resource):
